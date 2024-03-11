cb2cdfb481fc4fd875b303f1722d80faabe7d133
asmaier <asmaier@web.de>
2024-03-11 11:03:24 +0100

    Connected markdown readme to setup.py, because since 2018 Pypi supports markdown descriptions directly, see https://dustingram.com/articles/2018/03/16/markdown-descriptions-on-pypi/ . Deleted upload script using pandoc for conversion, because it is not needed anymore.

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
 from setuptools import setup, find_packages
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
 setup(name='haddock-curses',
-	  version='0.1.5',
-	  description='Random curses from Captain Haddock.',
-	  long_description=readme(),
-	  classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-	'Natural Language :: English',
-	'Natural Language :: French',
-	'Natural Language :: German',
-        'Programming Language :: Python :: 2.7',
-	'Programming Language :: Python :: 3',
-        'Topic :: Utilities',
+      version='0.1.5',
+      description='Random curses from Captain Haddock.',
+      long_description='README.md',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Natural Language :: French',
+          'Natural Language :: German',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Utilities',
       ],
-	  url='https://github.com/asmaier/haddock',
-	  author='asmaier',
-	  author_email='asmaier@web.de',
-	  license='MIT',
-	  packages=find_packages(),
-	  include_package_data=True,
-	  zip_safe=False)
+      url='https://github.com/asmaier/haddock',
+      author='asmaier',
+      author_email='asmaier@web.de',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
--- upload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# see https://stackoverflow.com/questions/26737222/pypi-description-markdown-doesnt-work
-pandoc --columns=100 --from=markdown --to=rst --output=README.rst README.md
-python3 setup.py sdist upload
